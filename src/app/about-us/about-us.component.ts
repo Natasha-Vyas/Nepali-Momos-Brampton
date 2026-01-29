5a41bcc11f7c0629c6fcaf0c52f2bbee34aca654
Aman Kumar <aman.kumar@gkcfoods.com>
2026-01-29 16:09:56 +0530

    Nepali momo changes in root page and Grab & Go  page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -9,6 +9,21 @@ import { SeoService } from '../services/seo.service';
 })
 export class AboutUsComponent implements OnInit {
   hero: any = {};
+  cartCount = 0;
+
+  retail50 = [
+    { name: 'Veggi', desc: 'Includes famed chutney', price: '$45' },
+    { name: 'Ckn', desc: 'Includes famed chutney', price: '$50' },
+    { name: 'Paneer', desc: 'Includes famed chutney', price: '$55' },
+    { name: 'Malai Paneer', desc: 'Includes famed chutney', price: '$60' }
+  ];
+
+  retail15 = [
+    { name: 'Veggi', price: '$15' },
+    { name: 'Ckn', price: '$15' },
+    { name: 'Paneer', price: '$16' },
+    { name: 'Malai Paneer', price: '$18' }
+  ];
 
   constructor(
     private appService: AppService,
